3bbd0f80bf17cc8f832f743456152968dff3c4f1
Juan Rojas <juanrojas1912@gmail.com>
2023-01-23 22:04:32 -0500

    flask wtf validation

--- intermediate-flask/forms.py
+++ intermediate-flask/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SelectField
+from wtforms.validators import InputRequired, Optional
 
 class AddEmployeeForm(FlaskForm):
   """for to create a new employee"""
-  name = StringField("Name")
-  province = SelectField("Province")
-  department = SelectField("Department")
-  salary = FloatField("Salary")
+  name = StringField("Name", validators=[InputRequired()])
+  province = SelectField("Province", validators=[InputRequired()])
+  department = SelectField("Department", validators=[InputRequired()])
+  salary = FloatField("Salary", validators=[InputRequired()])
