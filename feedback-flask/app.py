34a81e73a3f9e916dc3cfe321d64222f403a02e6
Juan Rojas <juanrojas1912@gmail.com>
2023-02-03 16:57:50 -0500

    feedback flask user model

--- feedback-flask/app.py
+++ feedback-flask/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from models import db, connect_db, User
 
 app =Flask(__name__)
+app.app_context().push()
 
+app.config["SQLALCHEMY_DATABASE_URI"]         = "postgresql:///feedbackdb"
 app.config["DEBUG"]                           = True
 app.config["TEMPLATES_AUTO_RELOAD"]           = True
 app.config["SECRET_KEY"]                      = "opalinula456!"
+
+connect_db(app)
 
 @app.route("/")
 def feedback_stream():
