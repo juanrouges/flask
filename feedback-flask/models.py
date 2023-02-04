67988eda249fe286f7f1693f0bb97fb652b3ed70
Juan Rojas <juanrojas1912@gmail.com>
2023-02-03 21:34:05 -0500

    feedback flask User class register user

--- feedback-flask/models.py
+++ feedback-flask/models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+
+bcrypt = Bcrypt()
 
 def connect_db(app):
     db.app = app
@@ -15,4 +18,11 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False, unique=True)
     last_name = db.Column(db.String(30), nullable=False, unique=True)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
 
